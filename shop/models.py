43283e5c5a18fcbd9342f2feb4b4c81d214742da
islam abdelmaaboud <islamabdelmaaboud@yahoo.com>
2017-12-19 14:51:59 +0200

    initial commit , add shop models

---
+++ shop/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.urlresolvers import reverse
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,db_index=True)
+    slug = models.SlugField(max_length=200,db_index=True,unique=True)
+
+    class Meta:
+        ordering=("name",)
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=[self.slug])
+
+
+class Product(models.Model):
+    caegory = models.ForeignKey(Category,related_name="products")
+    name = models.CharField(max_length=200,db_index=True)
+    slug = models.SlugField(max_length=200,db_index=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d",blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created",)
+        index_together=  (("id","slug"),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail",args=[self.id,self.slug])
